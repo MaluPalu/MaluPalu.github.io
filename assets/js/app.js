4bf5f0a5bcad1b648884f91dc7a51d1aba5ea0f7
LAPTOP-NHRLSVER\malup <malu.peralta808@gmail.com>
2018-10-05 21:41:30 -0700

    updating

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,22 +30,22 @@ $(".mySig").mouseleave(function() {
 
 let jQue = [
   {
-    name: "Vader Zag",
+    name: "Spork",
     img: "https://s-media-cache-ak0.pinimg.com/originals/6d/2d/b5/6d2db55ea73aceb13854e041b44023d1.jpg",
     url: "http://friv-games.com/games/darth-vader-games.html",
-    desc: "Project for 2-bit Darth Vader game"
+    desc: "A social recipe-sharing website where you can upload your own recipes, comment/like other user's recipes and follow other users whose recipes you enjoy."
   },
   {
-    name: "MTG Database",
+    name: "PubHub",
     img: "http://fc01.deviantart.net/fs70/i/2011/170/6/b/magic_the_gathering_dotp_2012_by_jenack-d3jcych.png",
     url: "http://magic.wizards.com/en",
-    desc: "Database for Magic the Gathering"
+    desc: "A public outdoor space locater/sharer where you can post your favorite public outdoor space for casual meetings, get-togethers or parties in San Fransisco City using Google Maps API."
   },
   {
-    name: "Youtube Blue",
+    name: "SoFit",
     img: "http://oi50.tinypic.com/2ur8bpj.jpg",
     url: "https://www.youtube.com/",
-    desc: "Project for new YouTube site"
+    desc: "A social network for people who enjoy tracking certain or all aspects of their fitness lifestyle, whether youre a swimmer, runner, biker or even weight lifter you have real-time data representation of your goals and accomplishments. "
   }
 ];
 for (let i = 0; i < jQue.length; i++) {
@@ -87,13 +87,13 @@ for (let i = 0; i < jQue.length; i++) {
 //animate projectDesc to reveal once clicked
 $(".imageContainer").on("click", function() {
  $(this).next(".projDesc").animate({
-   "height": "100%"
+     "height": "100%"
  });
 });
 //animate projDesc to hide when clicked again
 $(".projDesc").on("click", function() {
  $(this).animate({
-   "height": "0%"
+     "height": "0%"
  });
 });
 $(".dropdownbutton").on("click", function() {
